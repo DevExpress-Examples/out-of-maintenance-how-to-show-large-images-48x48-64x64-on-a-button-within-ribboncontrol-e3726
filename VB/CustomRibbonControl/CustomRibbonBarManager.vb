d4b07d54b69686fbcac8941ac3508d79312173ac
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomRibbonControl/CustomRibbonBarManager.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.XtraBars.Ribbon
+Imports DevExpress.XtraBars.ViewInfo
+Imports DevExpress.XtraBars.Ribbon.ViewInfo
+Imports DevExpress.XtraBars
+
+Namespace LargeRibbonButton.CustomRibbonControl
+	Friend Class CustomRibbonBarManager
+		Inherits RibbonBarManager
+		Public Sub New(ByVal ribbon As RibbonControl)
+			MyBase.New(ribbon)
+		End Sub
+		Protected Friend Shadows ReadOnly Property DrawParameters() As BarDrawParameters
+			Get
+				Return PaintStyle.DrawParameters
+			End Get
+		End Property
+	End Class
+End Namespace
